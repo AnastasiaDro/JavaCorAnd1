9b803bfb6d97950b8dd09c85976c20ec3579a70a
cerebus <adrogunova@gmail.com>
2021-12-11 18:29:46 +0300

    made ex06

--- src/main/java/ru/geekbrains/lesson2/Main.java
+++ src/main/java/ru/geekbrains/lesson2/Main.java
@@ -1,9 +1,39 @@
 package main.java.ru.geekbrains.lesson2;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.Random;
 
+
 public class Main {
+
+    public static int       getArrSum(int[] arr)
+    {
+        int res = 0;
+        for (int j : arr) {
+            res += j;
+        }
+        return res;
+    }
+
+    public static boolean   checkBalance(int[] arr)
+    {
+        int fSum = 0;   //сумма первого кусочка
+        int sSum = getArrSum(arr);   //сумма второго кусочка
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            fSum += arr[i];
+            sSum -= arr[i];
+            if (fSum == sSum)
+            {
+                System.out.println("sum = " + sSum);
+                return true;
+            }
+
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         //ex01
         //Задать целочисленный массив, состоящий из элементов 0 и 1.
@@ -68,5 +98,55 @@ public class Main {
             }
             System.out.println(Arrays.toString(squareArr[i]));
         }
+
+        //ex05
+        //** Задать одномерный массив и найти в нем
+        //   минимальный и максимальный элементы (без помощи интернета);
+        System.out.println("\n**ex05");
+        int min;
+        int max;
+        int[] arr5 = { 1, 6, 6, -1, -15, 20, 7};
+        System.out.println("arr5 = " + Arrays.toString(arr5));
+        min = arr5[0];
+        max = arr5[0];
+        for (int i = 1; i < arr5.length; i++) {
+            if (arr5[i] < min)
+                min = arr5[i];
+            if (arr5[i] > max)
+                max = arr5[i];
+        }
+        System.out.println("min = " + min + "\nmax =  " + max);
+
+        //ex06 - сам метод на строке 19
+        //** Написать метод, в который передается не пустой одномерный целочисленный массив,
+        //   метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой
+        //   части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+        //   checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||,
+        //   эти символы в массив не входят.
+        System.out.println("ex06");
+        //test1         true (from subject)
+        int[] testArr1 = { 2, 2, 2, 1, 2, 2, 10, 1 };
+        System.out.println("testArr1 (expected - true): " + Arrays.toString(testArr1));
+        System.out.println(checkBalance(testArr1));
+        //test2         true (from subject)
+        int[] testArr2 = { 1, 1, 1, 2, 1 };
+        System.out.println("testArr2 (expected - true): " + Arrays.toString(testArr2));
+        System.out.println(checkBalance(testArr2));
+        //test3         true (checking validity of extreme values: index 0 and other)
+        int[] testArr3 = { 5, 1, 1, 2, 1 }; //
+        System.out.println("testArr3 (expected - true): " + Arrays.toString(testArr3));
+        System.out.println(checkBalance(testArr3));
+        //test4         true (checking validity of extreme values: last index and other)
+        int[] testArr4 = { 2, 1, 1, 0, 2, 6 };
+        System.out.println("testArr4 (expected - true): " + Arrays.toString(testArr4));
+        System.out.println(checkBalance(testArr4));
+        //test5         false
+        int[] testArr5 = { 2, 1, 1, 8, 8, 6 };
+        System.out.println("testArr5 (expected - false): " + Arrays.toString(testArr5));
+        System.out.println(checkBalance(testArr5));
+        //test6         false
+        int[] testArr6 = { 239, 1, 17, 456356738, 8, 6 };
+        System.out.println("testArr6 (expected - false): " + Arrays.toString(testArr6));
+        System.out.println(checkBalance(testArr6));
     }
 }
