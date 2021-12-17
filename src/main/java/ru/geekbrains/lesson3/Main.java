7374a11e8abda0dcada4bdb7c1082b26072cfff3
AnastasiaDro <adrogunova@gmail.com>
2021-12-17 03:20:55 +0300

    Done!

---
+++ src/main/java/ru/geekbrains/lesson3/Main.java
@@ -0,0 +1,41 @@
+package main.java.ru.geekbrains.lesson3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner      sc;
+        StringBuilder       code;
+        String       answer;
+        int          cycleIter;
+        Random       rand;
+        int         r;
+        char        c;
+        sc = new Scanner(System.in);
+        code = new StringBuilder("###############");
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        rand = new Random();
+        r    = rand.nextInt(words.length);
+        System.out.print("Guess the word game is starting!");
+        System.out.println("For exit type: \"EXIT\"");
+        System.out.println("The variants:\n\"apple\", \"orange\", \"lemon\", \"banana\",\n\"apricot\", \"avocado\", \"broccoli\", \"carrot\"\n\"cherry\", \"garlic\", \"grape\", \"melon\"\n\"leak\", \"kiwi\", \"mango\", \"mushroom\"\n\"nut\", \"olive\", \"pea\", \"peanut\"\n\"pear\", \"pepper\", \"pineapple\", \"pumpkin\"\n\"potato\"");
+        //ТАК ЛЕГЧЕ ПРОВЕРИТЬ
+        System.out.println("DEBUG: " + words[r]);
+        while(true) {
+            System.out.print("Your answer: ");
+            answer = sc.nextLine();
+            if (answer.equals(words[r]) || answer.equals("EXIT"))
+                break;
+            cycleIter = Math.min(answer.length(), words[r].length());
+            for (int i = 0; i < cycleIter; i++) {
+                c = answer.charAt(i);
+                if (c == words[r].charAt(i))
+                    code.setCharAt(i, c);
+            }
+            System.out.println("Current status: " + code);
+        }
+        System.out.print("YOU ARE WIN!");
+    }
+}
