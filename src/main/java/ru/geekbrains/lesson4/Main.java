7d2e862997986c11a3cf7477d65bf056a77de49c
cerebus <adrogunova@gmail.com>
2021-12-23 16:24:29 +0300

    made winChecking

--- src/main/java/ru/geekbrains/lesson4/Main.java
+++ src/main/java/ru/geekbrains/lesson4/Main.java
@@ -18,10 +18,9 @@ public class Main {
     public static int COMP_WIN = 2;
     public static int first = 0; //0 - первым ходит человек, 1 - первым ходит компьютер
     static Random rand;
-    public static int winner;
+    public static int winner = -1;
 
-    public static void init()
-    {
+    public static void init() {
         scan = new Scanner(System.in);
         System.out.print("Введите размерность поля x: ");
         sizeX = scan.nextInt();
@@ -32,8 +31,7 @@ public class Main {
         targetWinSum = sizeX;
     }
 
-    public static void fillArea()
-    {
+    public static void fillArea() {
         System.out.println("Поле готово!");
         for (int i = 0; i < sizeX; i++) {
             for (int j = 0; j < sizeY; j++) {
@@ -44,8 +42,7 @@ public class Main {
         }
     }
 
-    public static void printArea()
-    {
+    public static void printArea() {
         System.out.println("Поле: ");
         for (int i = 0; i < sizeX; i++) {
             for (int j = 0; j < sizeY; j++) {
@@ -59,20 +56,19 @@ public class Main {
         first = rand.nextInt(2);
         if (first == 0) {
             System.out.println("Ты шагаешь первым! Символ - X");
-            COMP_CHAR = 'Y';
+            COMP_CHAR = '0';
         } else {
             System.out.println("Ты шагаешь вторым! Символ - 0");
-            USER_CHAR = 'Y';
+            USER_CHAR = '0';
         }
     }
 
     public static void makeStep(int x, int y, char c) {
         arr[x][y] = c;
-       // printArea();
+        printArea();
     }
 
-    public static void stepToEmpty(int n, int coord, int size)
-    {
+    public static void stepToEmpty(int n, int coord, int size) {
         int f = 0;
         int s = 0;
         if (coord == COORD_Y)
@@ -87,24 +83,46 @@ public class Main {
         }
     }
 
-    public static void makeRandomStep()
-    {
-        int x = rand.nextInt(sizeX);
-        int y = rand.nextInt(sizeY);
+    public static void makeRandomStep() {
+        int x = -1;
+        int y = -1;
+        do {
+            x = rand.nextInt(sizeX);
+            y = rand.nextInt(sizeY);
+        } while (!checkCoords(x, y));
         makeStep(x, y, COMP_CHAR);
         printArea();
     }
 
-    public
+    public static void makeFirstCompStep() {
+        int x;
+        int y;
+        int var = rand.nextInt(2);
+        if (var == 0) {
+            makeRandomStep();
+            return;
+        } else {
+            if (sizeX % 2 > 0)
+                x = sizeX / 2 + 1;
+            else {
+                x = sizeX / 2;
+            }
+            if (sizeY % 2 > 0)
+                y = sizeY / 2 + 1;
+            else {
+                y = sizeY / 2;
+            }
+        }
+        makeStep(x, y, COMP_CHAR);
+    }
+
 
     public static boolean checkCoords(int x, int y) {
         return arr[x][y] == '?';
     }
 
 
-
-    public static void makeUserStep()
-    {
+    public static void makeUserStep() {
         int x;
         int y;
 
@@ -120,6 +138,62 @@ public class Main {
         printArea();
     }
 
+    public static void makeCompStep() {
+
+    }
+
+    public static boolean isWin(char[][] arr) {
+        int userSteps = 0;
+        int compSteps = 0;
+
+        //проверим все горизонтальные линии
+        for (int i = 0; i < sizeY; i++) {
+            for (int j = 0; j < sizeX; j++) {
+                if (arr[i][j] == '?')
+                    break;
+                if (arr[i][j] == COMP_CHAR)
+                    compSteps++;
+                else
+                    userSteps++;
+            }
+            if (compSteps == sizeX) {
+                winner = COMP_WIN;
+                return true;
+            }
+            if (userSteps == sizeX) {
+                winner = USER_WIN;
+                return true;
+            }
+            compSteps = 0;
+            userSteps = 0;
+        }
+
+
+        //проверим всё по вертикали
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                if (arr[y][x] == '?')
+                    break;
+                if (arr[y][x] == USER_CHAR)
+                    userSteps++;
+                else
+                    compSteps++;
+            }
+            if (userSteps == sizeY) {
+                winner = USER_WIN;
+                return true;
+            }
+            if (compSteps == sizeY) {
+                winner = COMP_WIN;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
     public static void main(String[] args) {
         init();
         fillArea();
@@ -127,13 +201,21 @@ public class Main {
 
         int counter = 0;
         int min = Math.min(sizeX, sizeY);
-        if (COMP_CHAR== 'X') {
-            makeRandomStep();
-        }
-        makeUserStep();
+//        if (COMP_CHAR== 'X') {
+//            makeFirstCompStep();
+//        }
+//        do {
+//            makeUserStep();
+//            makeCompStep();
+//        } while (!isWin());
 
+        char[][] arrtest = { {'X',    '?',   'X'},
+                            {'?', '?', '?'},
+                            {'?', '?', '?'}};
+        System.out.println(isWin(arrtest));
 
-        }
         String results = winner == USER_WIN ? "Ты выиграл! Ура!" : "Искусственный интеллект победил! Восстание машин!";
+        System.out.println(results);
     }
+}
 
